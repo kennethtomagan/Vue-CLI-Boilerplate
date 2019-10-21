f5026477f55806319b26f136b2adabeea1e1cf82
kenneth <kenenth.tomagan123@gmail.com>
2019-10-21 19:16:24 +0800

    auth setup

---
+++ src/auth.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import router from './router'
+import moment from "moment";
+
+Vue.router = router
+Vue.use(VueAxios, axios);
+
+Vue.axios.defaults.baseURL = process.env.VUE_APP_API_URL;
+Vue.use(require('@websanova/vue-auth'), {
+    auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+    http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+    router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+    fetchData: { url: `profile`, method: 'GET', enabled: true, authType: 'bearer' },
+    refreshData: { url: `profile`, method: 'GET', enabled: true, interval: 30, authType: 'bearer' },
+    authRedirect: { path: '/' },
+    // notFoundRedirect: { path: '/' },
+})
